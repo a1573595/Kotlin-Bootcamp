2fbc941790ce3a0e823a5b6fbd7a48cead3eea52
a1573595 <a1573595@gmail.com>
2019-05-25 17:48:03 +0800

    finish DiceRoller

--- DiceRoller/app/src/main/java/a1573595/com/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/a1573595/com/diceroller/MainActivity.kt
@@ -2,11 +2,35 @@ package a1573595.com.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_roll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6)+1
+
+        Toast.makeText(this, "$randomInt", Toast.LENGTH_SHORT).show()
+
+        img_dice.setImageResource(when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        })
     }
 }
